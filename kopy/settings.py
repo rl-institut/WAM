219b2ef32fc254b2e45206075e1731a1770b20cb
hendrik <hendrik.huyskens@rl-institut.de>
2018-03-27 17:24:04 +0200

    Changed db config path to use environment variable

--- kopy/settings.py
+++ kopy/settings.py
@@ -16,12 +16,8 @@ from configobj import ConfigObj
 # Pyomo objects must be loaded in main thread and are imported from here:
 from oemof.solph.plumbing import _Sequence as Sequence
 
-DB_CONFIG_FILE = os.path.join(
-    os.path.expanduser("~"),
-    '.python_local',
-    'db_config.cfg'
-)
-db_config = ConfigObj(DB_CONFIG_FILE)
+
+db_config = ConfigObj(os.environ['DB_CONFIG_PATH'])
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
