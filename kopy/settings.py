f627e08dad15d3771859a584709edd53b6a758c8
hendrik <hendrik.huyskens@rl-institut.de>
2018-03-27 17:26:28 +0200

    Added docker host

--- kopy/settings.py
+++ kopy/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'testserver', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', 'testserver', '127.0.0.1', '172.18.0.3']
 
 
 # Application definition
