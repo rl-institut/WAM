60d5ab8d34c087c9862be951af9d471aa5e5747f
Ludee <Ludwig.Huelk@rl-institut.de>
2018-05-07 17:07:11 +0200

    change default database

--- kopy/settings.py
+++ kopy/settings.py
@@ -80,12 +80,20 @@ WSGI_APPLICATION = 'kopy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+'''
+DATABASES = {
     'default': config['KOPERNIKUS'],
     'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
