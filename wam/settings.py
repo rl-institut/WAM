2c51a0b3c2ef31d8342a191adf7b8057fc7a8944
hendrik <hendrik.huyskens@rl-institut.de>
2018-11-02 13:51:51 +0100

    Added ALLOWED_HOSTS and DEBUG to envs

--- wam/settings.py
+++ wam/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['localhost', 'testserver', '127.0.0.1', '172.18.0.3']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 # Additional apps are loaded from environment variable
 WAM_APPS = os.environ['WAM_APPS'].split(',')
