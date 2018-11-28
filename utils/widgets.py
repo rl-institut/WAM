3f703a02ed8ac5a16e30c099dc6f797db30468fb
hendrik <hendrik.huyskens@rl-institut.de>
2018-11-28 09:37:30 +0100

    Added docstrings for InfoButton

--- utils/widgets.py
+++ utils/widgets.py
@@ -6,10 +6,27 @@ from django.forms.renderers import get_default_renderer
 
 
 class InfoButton(object):
+    """
+    Clickable icon "?" which opens reveal window showing info text
+
+    Displays circled question mark. Hovering question mark will show tooltip,
+    clicking on will open "reveal" window showing info text
+    (markdown supported).
+    """
+
     template_name = 'widgets/help_button.html'
     counter = count()
 
     def __init__(self, text, tooltip, is_markdown=False):
+        """
+
+        Parameters
+        ----------
+        text (str): Info text will appear in window. If text is written in
+            markdown, parameter `is_markdown` must be set to True.
+        tooltip (str): Text shown when hovering question mark icon
+        is_markdown (bool): If set, text will be rendered as markdown into html
+        """
         self.id = next(self.counter)
         self.text = markdown(text) if is_markdown else text
         self.tooltip = tooltip
