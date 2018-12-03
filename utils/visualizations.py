0167c9d47ec161d147aa3910d49b5430d0a8a87c
hendrik <hendrik.huyskens@rl-institut.de>
2018-12-03 13:18:35 +0100

    Refactored visualizations to render template

--- utils/visualizations.py
+++ utils/visualizations.py
@@ -3,19 +3,31 @@ import itertools
 from abc import ABC, abstractmethod
 
 from django.utils.safestring import mark_safe
+from django.forms.renderers import get_default_renderer
+from django.template.exceptions import TemplateDoesNotExist
 
 
 class VisualizationTemplate(ABC):
     id_counter = itertools.count()
+    template_name = None
 
     def __init__(self, data=None):
         self.id = next(self.id_counter)
         if data is not None:
             self.set_data(data)
 
-    @abstractmethod
+    def get_context(self, **kwargs):
+        return {
+            'div_id': kwargs.get('div_id', f'rank_{self.id}'),
+            'div_kwargs': kwargs.get('div_kwargs', {})
+        }
+
     def render(self, **kwargs):
-        pass
+        if self.template_name is None:
+            raise TemplateDoesNotExist('No template_name set')
+        renderer = get_default_renderer()
+        context = self.get_context(**kwargs)
+        return mark_safe(renderer.render(self.template_name, context))
 
     @abstractmethod
     def set_data(self, data):
