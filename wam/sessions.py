f57b225ed86edac773ff5cc7dc502ef324d009bd
hendrik <hendrik.huyskens@rl-institut.de>
2019-01-03 14:38:02 +0100

    Removed stemp-specific UserSession
    Fixes https://github.com/rl-institut/WAM/issues/9

--- wam/sessions.py
+++ wam/sessions.py
@@ -1,15 +1,13 @@
-from stemp.user_data import UserSession
-
 
 class SessionData(object):
     def __init__(self):
         self.sessions = {}
 
-    def start_session(self, request):
+    def start_session(self, request, session_obj):
         if request.session.session_key is None:
             request.session.create()
         if request.session.session_key not in self.sessions:
-            self.sessions[request.session.session_key] = UserSession()
+            self.sessions[request.session.session_key] = session_obj()
 
     def get_session(self, request):
         user_session = self.sessions.get(request.session.session_key, None)
