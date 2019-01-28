7f83c238e745e0ed04e4cfedb9e2d7f2344d58f4
root <root@v22018032848563176.happysrv.de>
2019-01-28 14:18:46 +0100

    No error occurs when requirements.txt is not found

--- install_requirements.py
+++ install_requirements.py
@@ -6,5 +6,6 @@ base_dir = os.path.abspath(os.path.curdir)
 apps = os.environ['WAM_APPS'].split(',')
 for app in apps:
     req_file = os.path.join(base_dir, app, 'requirements.txt')
-    subprocess.call(['/opt/conda/envs/django/bin/pip', 'install', '-r', req_file])
+    if os.path.isfile(req_file):
+        subprocess.call(['/opt/conda/envs/django/bin/pip', 'install', '-r', req_file])
 
