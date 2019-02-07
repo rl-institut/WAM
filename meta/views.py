f42bb7231882f5131bdc285df68fc153b0359685
hendrik <hendrik.huyskens@rl-institut.de>
2019-02-07 14:19:33 +0100

    Refactored SourcesView to inherit from AppList
    This aims to reduce code-duplications for assumptions

--- meta/views.py
+++ meta/views.py
@@ -4,15 +4,68 @@ from meta import models
 from wam.settings import BASE_DIR
 
 
-class SourcesView(ListView):
-    """Lists all sources grouped by source category.
+class AppListView(ListView):
+    """Lists all entries of an app-related model
 
-    Sources can be filtered by app_name attribute. If base template is given
+    Model can be filtered by app_name attribute. If base template is given
     within specified app, SourceView extends this template. The base template
     is assumed to be located at
     <WAM_base_dir>/<app_name>/templates/<app_name>/base.html .
 
-    if base template is not found, base template from meta app is taken.
+    If base template is not found, base template from meta app is taken.
+    """
+    app_name = None
+    ordering = ['category__name']
+    model = None
+
+    def __init__(self, *args, **kwargs):
+        super(AppListView, self).__init__(*args, **kwargs)
+        if self.app_name is not None:
+            self.queryset = self.model.objects.filter(
+                app_name=self.app_name)
+        else:
+            self.queryset = self.model.objects.all()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(AppListView, self).get_context_data(
+            object_list=object_list, **kwargs)
+
+        # Try to load base.html template from app:
+        base_template = 'meta/base.html'
+        if self.app_name is not None:
+            base_template_file = os.path.join(
+                BASE_DIR,
+                self.app_name,
+                'templates',
+                self.app_name,
+                'base.html'
+            )
+            if os.path.exists(base_template_file):
+                base_template = self.app_name + '/base.html'
+        context['base_template'] = base_template
+
+        # Highlight specific id:
+        highlight_id = self.request.GET.get(self.model._meta.model_name, None)
+        if (
+                highlight_id is not None and
+                context['object_list'].filter(pk=highlight_id).exists()
+        ):
+            context['highlight'] = str(highlight_id)
+
+        # Highlight category:
+        category_id = self.request.GET.get('category', None)
+        if (
+                category_id is not None and
+                context['object_list'].filter(
+                    category__pk=category_id).exists()
+        ):
+            context['category'] = str(category_id)
+
+        return context
+
+
+class SourcesView(AppListView):
+    """Lists all sources grouped by category.
 
     Usage:
         # in urls.py:
@@ -33,49 +86,35 @@ class SourcesView(ListView):
 
     """
     template_name = 'meta/sources.html'
-    app_name = None
-    ordering = ['category__name']
-
-    def __init__(self, *args, **kwargs):
-        super(SourcesView, self).__init__(*args, **kwargs)
-        if self.app_name is not None:
-            self.queryset = models.Source.objects.filter(
-                app_name=self.app_name)
-        else:
-            self.queryset = models.Source.objects.all()
+    model = models.Source
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(SourcesView, self).get_context_data(
             object_list=object_list, **kwargs)
 
-        # Try to load base.html template from app:
-        base_template = 'meta/base.html'
-        if self.app_name is not None:
-            base_template_file = os.path.join(
-                BASE_DIR,
-                self.app_name,
-                'templates',
-                self.app_name,
-                'base.html'
-            )
-            if os.path.exists(base_template_file):
-                base_template = self.app_name + '/base.html'
-        context['base_template'] = base_template
-
-        # Highlight source or category:
-        source_id = self.kwargs.pop('source', None)
-        if (
-                source_id is not None and
-                context['object_list'].filter(pk=source_id).exists()
-        ):
-            context['source'] = str(source_id)
-
-        category_id = self.kwargs.pop('category', None)
-        if (
-                category_id is not None and
-                context['object_list'].filter(
-                    category__pk=category_id).exists()
-        ):
-            context['category'] = str(category_id)
-
         return context
+
+
+class AssumptionsView(AppListView):
+    """Lists all assumptions grouped by category.
+
+    Usage:
+        # in urls.py:
+
+        from meta.models import AssumptionsView
+
+        urlpatterns = [
+            ...
+            path('assumptions', AssumptionsView.as_view(app_name='stemp')),
+            ...
+        ]
+
+    Notes
+    -----
+    * Models are defined in :mod:`~.meta.models`
+    * The assumptions template displays all assumptions in a table,
+    each row has a unique id
+
+    """
+    template_name = 'meta/assumptions.html'
+    model = models.Assumption
