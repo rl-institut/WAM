5cb6bc65897a8e846aea6f36e9fb9732442fc0d6
hendrik <hendrik.huyskens@rl-institut.de>
2019-02-27 13:15:54 +0100

    Implemented get_app_from_request function

--- utils/shortcuts.py
+++ utils/shortcuts.py
@@ -9,3 +9,12 @@ def redirect_with_get_parameters(url, **parameters):
     response['Location'] += '?' + '&'.join(
         [str(key) + '=' + str(value) for key, value in parameters.items()])
     return response
+
+
+def get_app_from_request(request):
+    try:
+        return request.path.split('/')[1]
+    except AttributeError:
+        raise AttributeError(
+            'Current app could not be found'
+        )
