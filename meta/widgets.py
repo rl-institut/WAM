ecddaff5027d1e2b855c24e440aab47167f96130
nesnoj <jonathan.amme@rl-institut.de>
2019-04-05 11:29:17 +0200

    meta: change layout of source's json popup
    add indented divs of sub-elements to get a better visual structure

--- meta/widgets.py
+++ meta/widgets.py
@@ -1,4 +1,3 @@
-
 from django.utils.safestring import mark_safe
 
 
@@ -6,16 +5,22 @@ class JsonWidget(object):
     def __init__(self, json):
         self.json = json
 
-    def __convert_to_html(self, data):
+    def __convert_to_html(self, data, level=0):
         html = ''
         if isinstance(data, dict):
+            html += f'<div style="margin-left: {level*2}rem;' \
+                    f'margin-bottom: 0.75rem;' \
+                    f'padding-left: 0.5rem;' \
+                    f'border-left: 1px dotted #002E4F;">'\
+                if level > 0 else '<div>'
             for key, value in data.items():
-                html += f'<b>{key}:</b> {self.__convert_to_html(value)}'
+                html += f'<b>{key}:</b> {self.__convert_to_html(value, level+1)}'
+            html += '</div>'
         elif isinstance(data, list):
-            html += '<ul>'
+            html += f'<div style="margin-left: {level*2}rem;">'
             for item in data:
-                html += f'<li>{self.__convert_to_html(item)}</li>'
-            html += '</ul>'
+                html += f'{self.__convert_to_html(item, level+1)}'
+            html += '</div>'
         else:
             html += f'{data}<br>'
         return html
@@ -26,4 +31,4 @@ class JsonWidget(object):
             header += f'<p class="lead">{self.json["title"]}</p>'
         if self.json["description"] != "":
             header += f'<p>{self.json["description"]}</p>'
-        return mark_safe(header + self.__convert_to_html(self.json))
+        return mark_safe(header + self.__convert_to_html(data=self.json))
