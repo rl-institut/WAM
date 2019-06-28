70a2ed60e667f1e24728cdb96bd4bd7a0952d8c8
nesnoj <jonathan.amme@rl-institut.de>
2019-06-29 01:45:16 +0200

    add fct to send mail via exchange server

---
+++ utils/mail.py
@@ -0,0 +1,59 @@
+import logging
+from requests.exceptions import ConnectionError
+
+from exchangelib import Credentials, Account, Message, Mailbox
+from exchangelib.errors import AutoDiscoverFailed
+from wam.settings import WAM_EXCHANGE_ACCOUNT, WAM_EXCHANGE_EMAIL, WAM_EXCHANGE_PW
+
+
+def send_email(to_email, app_name, from_name, from_email, subject, message):
+    """Send E-mail via MS Exchange Server using credentials from env vars"""
+
+    credentials = Credentials(WAM_EXCHANGE_ACCOUNT,
+                              WAM_EXCHANGE_PW)
+    try:
+        account = Account(WAM_EXCHANGE_EMAIL,
+                          credentials=credentials,
+                          autodiscover=True)
+    except ConnectionError:
+        err_msg = 'Feedback-Form - Verbindungsfehler!'
+        logging.error(err_msg)
+        return False
+    except AutoDiscoverFailed:
+        err_msg = 'Feedback-Form - Konto- oder Authentifinzierungsfehler!'
+        logging.error(err_msg)
+        return False
+    except Exception as err:
+        err_msg = f'Feedback-Form - Sonstiger Fehler: {err}'
+        logging.error(err_msg)
+        return False
+
+    recipients = [Mailbox(email_address=to_email)]
+
+    subject_notification = f'Nachricht über WAM Feedback-Formular: ' \
+                           f'App {app_name}'
+    body = f'Sie haben eine Nachricht über das Feedback-Formular der WAM ' \
+           f'erhalten.\n\n' \
+           f'App: {app_name}\n' \
+           f'Absender: {from_name} ({from_email})\n' \
+           f'Betreff: {subject}\n' \
+           f'====================\n' \
+           f'{message}\n' \
+           f'====================\n' \
+           f'Bitte antworte nicht auf diese E-Mail, junger PadaWAM!\n' \
+           f'Gez. Obi WAM Kenobi'
+
+    m = Message(account=account,
+                folder=account.sent,
+                subject=subject_notification,
+                body=body,
+                to_recipients=recipients)
+
+    try:
+        m.send_and_save()
+    except Exception as err:
+        err_msg = f'Feedback-Form - Fehler beim Mailversand: {err}'
+        logging.error(err_msg)
+        return False
+
+    return True
