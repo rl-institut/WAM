e7da1bad1c24e43a2f2a10739355907ef7a5edd8
hendrik <hendrik.huyskens@rl-institut.de>
2019-08-23 14:45:15 +0200

    Database and Celery Setup is skipped in RTD-mode

--- wam/celery.py
+++ wam/celery.py
@@ -7,13 +7,18 @@ from wam.settings import config
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wam.settings')
 
-url = 'amqp://{user}:{password}@{host}:{port}/{vhost}'.format(
-    user=config['CELERY']['USER'],
-    password=config['CELERY']['PASSWORD'],
-    host=config['CELERY']['HOST'],
-    port=config['CELERY']['PORT'],
-    vhost=config['CELERY']['VHOST'],
-)
+rtd_mode = config['WAM'].get('RTD-MODE', 'False') == 'True'
+if rtd_mode:
+    url = None
+else:
+    url = 'amqp://{user}:{password}@{host}:{port}/{vhost}'.format(
+        user=config['CELERY']['USER'],
+        password=config['CELERY']['PASSWORD'],
+        host=config['CELERY']['HOST'],
+        port=config['CELERY']['PORT'],
+        vhost=config['CELERY']['VHOST'],
+    )
+
 app = Celery(
     'wam',
     broker=url,
@@ -26,7 +31,6 @@ app.conf.task_default_queue = 'wam_queue'
 app.conf.update(
     result_expires=3600,
 )
-
 
 if __name__ == '__main__':
     app.start()
