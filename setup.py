198b564a1663c5c1e3a7c093ca757a21a113b934
hendrik <hendrik.huyskens@rl-institut.de>
2019-08-23 14:51:09 +0200

    Added requirements to setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,13 @@
 
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='wam',
     version='1.0',
     packages=find_packages(),
+    install_requires=requirements,
     scripts=['manage.py']
 )
