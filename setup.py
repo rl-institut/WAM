7439a98b3947bddb9440e25be79fa7cdf1ddf4ee
hendrik <hendrik.huyskens@rl-institut.de>
2019-08-23 13:39:25 +0200

    Added minimal setup.py for packaging WAM-Server
    In order to support RTD

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='WAM-Server',
+    version='1.0',
+    packages=find_packages(),
+    scripts=['manage.py']
+)
