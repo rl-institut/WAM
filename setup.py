e87d324081e0b7f60b672a975c2675a3f7cddaa7
hendrik <hendrik.huyskens@rl-institut.de>
2019-08-23 15:01:06 +0200

    Added dependencies to setup.py (to install packages from git repo)

--- setup.py
+++ setup.py
@@ -4,11 +4,18 @@ from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
+    dependencies = {
+        requirement
+        for requirement in requirements
+        if requirement.startswith('git')
+    }
+    requirements = set(requirements) - dependencies
 
 setup(
     name='wam',
     version='1.0',
     packages=find_packages(),
     install_requires=requirements,
+    dependency_links=dependencies,
     scripts=['manage.py']
 )
