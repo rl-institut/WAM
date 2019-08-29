0bfed94f3b9273a7257d042634dc940b4f956bf0
hendrik <hendrik.huyskens@rl-institut.de>
2019-08-29 17:23:02 +0200

    Used '-e' for git requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
-    dependencies = [req for req in requirements if req.startswith('git')]
+    dependencies = [req.lstrip('-e ') for req in requirements if req.startswith('-e')]
     requirements = list(set(requirements) - set(dependencies))
 
 setup(
