972faddffa4fe2356f4badace8d42602a090345a
nesnoj <jonathan.amme@rl-institut.de>
2019-09-16 11:13:49 +0200

    update license version, add some more meta info

--- setup.py
+++ setup.py
@@ -12,8 +12,12 @@ with open('requirements.txt') as f:
 
 setup(
     name='wam',
-    version='1.0',
+    version='0.1.3',
     packages=find_packages(),
+    license='GNU Affero General Public License v3.0',
+    author='henhuy, nesnoj, Bachibouzouk, christian-rli, 4lm',
+    description='WAM - Web Applications & Maps',
+    url='https://github.com/rl-institut/WAM',
     install_requires=normal_requirements + git_requirements,
     scripts=['manage.py']
 )
